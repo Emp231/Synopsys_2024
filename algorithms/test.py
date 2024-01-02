afe0435d7007a2daa23bb63e68b48c82c571affd
sbalaji524 <siddharthbalaji6@gmail.com>
2024-01-01 22:50:49 -0800

    step 0 and 1 completed
    yet to check functionality

--- algorithms/test.py
+++ algorithms/test.py
@@ -17,12 +17,88 @@ import numpy as np
 def calc_cell_height(x, y):
   return elevation_height[x,y] + water_height[x,y]
 
-def is_do_nothing(water_height):
-    for x in range(water_height.shape[0]):
-        for y in range(water_height.shape[1]):
-            if water_height[x, y] > 0:
+def is_do_nothing(water_height, x, y):
+    if water_height[x,y] == 0:
+        return True
+    
+    return False
+
+def end_sim(water_height):
+    array_x = np.size(water_height, 1)
+    array_y = np.size(water_height, 0)
+    for x in range(array_x):
+        for y in range(array_y):
+            if water_height[x,y] != 0:
                 return False
+
     return True
+
+def is_ponding(water_height, elevation_height, x, y):
+    right_neighbor_elevation_height = -1
+    left_neighbor_elevation_height = -1
+    up_neighbor_elevation_height = -1
+    down_neighbor_elevation_height = -1
+    num = 0
+
+    current_cell_elevation_height = elevation_height[x, y]
+
+    if find_neighbor(x, y, "right", elevation_height) != -1:
+        right_neighbor_elevation_height = elevation_height[find_neighbor(x, y, "right", elevation_height), y]
+
+    if find_neighbor(x, y, "left", elevation_height) != -1:
+        left_neighbor_elevation_height = elevation_height[find_neighbor(x, y, "left", elevation_height), y]
+
+    if find_neighbor(x, y, "up", elevation_height) != -1:
+        up_neighbor_elevation_height = elevation_height[x, find_neighbor(x, y, "up", elevation_height)]
+
+    if find_neighbor(x, y, "down", elevation_height) != -1:
+        down_neighbor_elevation_height = elevation_height[x, find_neighbor(x, y, "down", elevation_height)]
+
+
+    if right_neighbor_elevation_height != -1 and current_cell_elevation_height < right_neighbor_elevation_height:
+        num += 1
+    if left_neighbor_elevation_height != -1 and current_cell_elevation_height < left_neighbor_elevation_height:
+        num += 1
+    if up_neighbor_elevation_height != -1 and current_cell_elevation_height < up_neighbor_elevation_height:
+        num += 1
+    if down_neighbor_elevation_height != -1 and current_cell_elevation_height < down_neighbor_elevation_height:
+        num += 1
+
+
+    if num == 4:
+        temp_array = [height for height in [right_neighbor_elevation_height, left_neighbor_elevation_height, up_neighbor_elevation_height, down_neighbor_elevation_height] if height != -1]
+        min_height = min(temp_array)
+        difference = min_height - elevation_height[x,y]
+        elevation_height[x,y] = min_height
+        water_height[x,y] -= difference
+  
+    
+def find_neighbor(x,y, direction, elevation_height_height):
+    array_x = np.size(water_height, 1)
+    array_y = np.size(water_height, 0)
+
+    if direction == "right":
+        if x < array_x - 1:
+            return x+1
+        else:
+            return -1
+    elif direction == "left":
+        if x > 0:
+            return x-1
+        else:
+            return -1
+    elif direction == "up":
+        if y > 0:
+            return y-1
+        else:
+            return -1
+    elif direction == "down":
+        if y < array_y - 1:
+            return x+1
+        else:
+            return -1
+    
+    
 
 
 elevation_height = np.array([[2540, 2548, 2525, 2530, 2530, 2534, 2512, 2522, 2538], [2543, 2522, 2530, 2530, 2521, 2520, 2527, 2509, 2519],
@@ -32,5 +108,3 @@ elevation_height = np.array([[2540, 2548, 2525, 2530, 2530, 2534, 2512, 2522, 25
                              [2539, 2549, 2506, 2541, 2507, 2522, 2503, 2543, 2547]])
 water_height = np.zeros((9,9))
 water_height[8,4] = 35
-
-print(is_do_nothing(water_height))
