67d98e0bb7e36f0a8368c9b8f18bbced86e15f81
Prasmit Kondawar <kondawarprasmit@gmail.com>
2024-01-03 12:22:30 -0800

    tried to implement gdal

---
+++ algorithms/caffé.py
@@ -0,0 +1,13 @@
+from osgeo import gdal
+dataset = gdal.Open("C:/Users/fastp/OneDrive/Documents/GitHub/Synopsys_2024/algorithms", gdal.GA_ReadOnly)
+
+print("Driver: {}/{}".format(dataset.GetDriver().ShortName,
+                            dataset.GetDriver().LongName))
+print("Size is {} x {} x {}".format(dataset.RasterXSize,
+                                    dataset.RasterYSize,
+                                    dataset.RasterCount))
+print("Projection is {}".format(dataset.GetProjection()))
+geotransform = dataset.GetGeoTransform()
+if geotransform:
+    print("Origin = ({}, {})".format(geotransform[0], geotransform[3]))
+    print("Pixel Size = ({}, {})".format(geotransform[1], geotransform[5]))
