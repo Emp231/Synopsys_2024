e59a5dfc1e8c1e5620373034c18a3461e5e83fec
sbalaji524 <siddharthbalaji6@gmail.com>
2024-01-02 22:47:39 -0800

    working on step 3

--- algorithms/test.py
+++ algorithms/test.py
@@ -5,7 +5,7 @@
  [2509 2533 2509 2530 2502 2514 2527 2510 2522]
  [2533 2525 2517 2532 2541 2517 2501 2503 2515]
  [2529 2548 2535 2528 2520 2519 2512 2525 2506]
- [2529 2500 2533 2506 2549 2523 2546 2509 2514]
+ [2529 2508 2533 2506 2549 2523 2546 2509 2514]
  [2513 2508 2518 2503 2526 2508 2530 2541 2511]
  [2539 2549 2506 2541 2507 2522 2503 2543 2547]]
 
@@ -94,6 +94,39 @@ def is_spreading_action(water_height, elevation_height, x, y):
     water_height[find_neighbor(x,y,"down", elevation_height), y] = split
     water_height[x,y] = split
     
+def is_increasing_level(water_height, elevation_height, x, y):
+    this_elevation_height = elevation_height[x,y]
+    right_neighbor_elevation_height = find_neighbor_elevation_height(x,y,"right", elevation_height)
+    left_neighbor_elevation_height = find_neighbor_elevation_height(x,y,"left", elevation_height)
+    up_neighbor_elevation_height = find_neighbor_elevation_height(x,y,"up", elevation_height)
+    down_neighbor_elevation_height = find_neighbor_elevation_height(x,y,"down", elevation_height)
+
+    equal_values_array = get_equal_neighbors_dirs(x,y,elevation_height)
+    original_dirs_array = np.array(["right", "left", "up", "down"])
+
+    if equal_values_array.size <= 3:
+        values_not_equal_array = np.setdiff1d(original_dirs_array, equal_values_array)
+        num = 0
+        if "right" in values_not_equal_array and right_neighbor_elevation_height > this_elevation_height:
+            num = num + 1
+
+        if "left" in values_not_equal_array and left_neighbor_elevation_height > this_elevation_height:
+            num = num + 1
+
+        if "up" in values_not_equal_array and up_neighbor_elevation_height > this_elevation_height:
+            num = num + 1
+
+        if "down" in values_not_equal_array and down_neighbor_elevation_height > this_elevation_height:
+            num = num + 1
+
+        if num == 4 - equal_values_array.size:
+            return True
+        else:
+            return False
+        
+# precondition is that the water_height of the central cell will always be greater than the increment constant
+def is_increasing_level_action(water_height, elevation_height, x, y, increment_constant):
+    print(get_equal_neighbors_dirs(x,y,elevation_height))
 
 def find_neighbor(x,y, direction, elevation_height):
     array_y = np.size(elevation_height, 1)
@@ -131,19 +164,53 @@ def find_neighbor_elevation_height(x,y, direction, elevation_height):
           return elevation_height[find_neighbor(x,y,direction,elevation_height), y]
         else:
             return -1
+        
+def get_equal_neighbors_dirs(x,y,elevation_height):
+    this_elevation_height = elevation_height[x,y]
+    right_neighbor_elevation_height = find_neighbor_elevation_height(x,y,"right", elevation_height)
+    left_neighbor_elevation_height = find_neighbor_elevation_height(x,y,"left", elevation_height)
+    up_neighbor_elevation_height = find_neighbor_elevation_height(x,y,"up", elevation_height)
+    down_neighbor_elevation_height = find_neighbor_elevation_height(x,y,"down", elevation_height)
+
+    equal_values_array = np.array([])
+
+    if this_elevation_height == right_neighbor_elevation_height:
+        equal_values_array = np.append(equal_values_array, "right")
+
+    if this_elevation_height == left_neighbor_elevation_height:
+        equal_values_array = np.append(equal_values_array, "left")
+
+    if this_elevation_height == up_neighbor_elevation_height:
+        equal_values_array = np.append(equal_values_array, "up")
+
+    if this_elevation_height == down_neighbor_elevation_height:
+        equal_values_array = np.append(equal_values_array, "down")
+
+    return equal_values_array
 
 
-elevation_height = np.array([[2540, 2548, 2525, 2530, 2530, 2534, 2512, 2522, 2538], [2543, 2533, 2530, 2530, 2521, 2520, 2527, 2509, 2519],
-                             [2533, 2533, 2533, 2523, 2523, 2536, 2507, 2510, 2527], [2509, 2533, 2509, 2530, 2502, 2514, 2527, 2510, 2522],
-                             [2533, 2525, 2517, 2532, 2541, 2517, 2501, 2503, 2515], [2529, 2548, 2535, 2528, 2520, 2519, 2512, 2525, 2506],
-                             [2529, 2500, 2533, 2506, 2549, 2523, 2546, 2509, 2514], [2513, 2508, 2518, 2503, 2526, 2508, 2530, 2541, 2511],
-                             [2539, 2549, 2506, 2541, 2507, 2522, 2503, 2543, 2547]])
+elevation_height = np.array([[2540, 2548, 2525, 2530, 2530, 2534, 2512, 2522, 2538], 
+                             [2543, 2533, 2530, 2530, 2521, 2520, 2527, 2509, 2519],
+                             [2533, 2533, 2533, 2523, 2523, 2536, 2507, 2510, 2527], 
+                             [2509, 2533, 2509, 2530, 2502, 2514, 2527, 2510, 2522],
+                             [2533, 2525, 2517, 2532, 2541, 2517, 2501, 2503, 2515], 
+                             [2529, 2548, 2535, 2528, 2520, 2519, 2512, 2525, 2506],
+                             [2529, 2508, 2533, 2506, 2549, 2523, 2546, 2509, 2514], 
+                             [2533, 2508, 2508, 2503, 2526, 2508, 2530, 2541, 2511],
+                             [2539, 2525, 2506, 2541, 2507, 2522, 2503, 2543, 2547]])
 water_height = np.zeros((9,9))
 water_height[6, 7] = 35
 water_height[2,1] = 15
+water_height[7,1] = 40
+increment_constant = 11
 """
 if is_ponding(water_height, elevation_height, 6,7):
     is_ponding_action(water_height, elevation_height, 6,7)
 """
+"""
 if is_spreading(water_height, elevation_height, 2, 1):
     is_spreading_action(water_height, elevation_height, 2, 1)
+"""
+if is_increasing_level(water_height, elevation_height, 7, 1):
+    #print(get_equal_neighbors_dirs(7,1,elevation_height))
+    is_increasing_level_action(water_height, elevation_height, 7, 1, increment_constant)
