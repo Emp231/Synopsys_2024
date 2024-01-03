2b7ac4605b059b81ac2b03af79ef0e91cc653162
sbalaji524 <siddharthbalaji6@gmail.com>
2024-01-02 19:20:41 -0800

    finished step 2
    last commit was supposed to say finished step 1

--- algorithms/test.py
+++ algorithms/test.py
@@ -1,8 +1,8 @@
 """
 [[2540 2548 2525 2530 2530 2534 2512 2522 2538]
- [2543 2522 2530 2530 2521 2520 2527 2509 2519]
- [2547 2533 2533 2523 2523 2536 2507 2510 2527]
- [2509 2529 2509 2530 2502 2514 2527 2510 2522]
+ [2543 2533 2530 2530 2521 2520 2527 2509 2519]
+ [2533 2533 2533 2523 2523 2536 2507 2510 2527]
+ [2509 2533 2509 2530 2502 2514 2527 2510 2522]
  [2533 2525 2517 2532 2541 2517 2501 2503 2515]
  [2529 2548 2535 2528 2520 2519 2512 2525 2506]
  [2529 2500 2533 2506 2549 2523 2546 2509 2514]
@@ -74,8 +74,26 @@ def is_ponding_action(water_height, elevation_height, x, y):
         water_height[x,y] -= difference
 
 
+def is_spreading(water_height, elevation_height, x, y):
+    right_neighbor_elevation_height = find_neighbor_elevation_height(x,y,"right", elevation_height)
+    left_neighbor_elevation_height = find_neighbor_elevation_height(x,y,"left", elevation_height)
+    up_neighbor_elevation_height = find_neighbor_elevation_height(x,y,"up", elevation_height)
+    down_neighbor_elevation_height = find_neighbor_elevation_height(x,y,"down", elevation_height)
 
-
+    
+    if right_neighbor_elevation_height == left_neighbor_elevation_height == up_neighbor_elevation_height == down_neighbor_elevation_height:
+        return True
+    else:
+        return False
+    
+def is_spreading_action(water_height, elevation_height, x, y):
+    split = water_height[x,y] / 5
+    water_height[x, find_neighbor(x,y,"right", elevation_height)] = split
+    water_height[x, find_neighbor(x,y,"left", elevation_height)] = split
+    water_height[find_neighbor(x,y,"up", elevation_height), y] = split
+    water_height[find_neighbor(x,y,"down", elevation_height), y] = split
+    water_height[x,y] = split
+    
 
 def find_neighbor(x,y, direction, elevation_height):
     array_y = np.size(elevation_height, 1)
@@ -115,13 +133,17 @@ def find_neighbor_elevation_height(x,y, direction, elevation_height):
             return -1
 
 
-elevation_height = np.array([[2540, 2548, 2525, 2530, 2530, 2534, 2512, 2522, 2538], [2543, 2522, 2530, 2530, 2521, 2520, 2527, 2509, 2519],
-                             [2547, 2533, 2533, 2523, 2523, 2536, 2507, 2510, 2527], [2509, 2529, 2509, 2530, 2502, 2514, 2527, 2510, 2522],
+elevation_height = np.array([[2540, 2548, 2525, 2530, 2530, 2534, 2512, 2522, 2538], [2543, 2533, 2530, 2530, 2521, 2520, 2527, 2509, 2519],
+                             [2533, 2533, 2533, 2523, 2523, 2536, 2507, 2510, 2527], [2509, 2533, 2509, 2530, 2502, 2514, 2527, 2510, 2522],
                              [2533, 2525, 2517, 2532, 2541, 2517, 2501, 2503, 2515], [2529, 2548, 2535, 2528, 2520, 2519, 2512, 2525, 2506],
                              [2529, 2500, 2533, 2506, 2549, 2523, 2546, 2509, 2514], [2513, 2508, 2518, 2503, 2526, 2508, 2530, 2541, 2511],
                              [2539, 2549, 2506, 2541, 2507, 2522, 2503, 2543, 2547]])
 water_height = np.zeros((9,9))
 water_height[6, 7] = 35
-
+water_height[2,1] = 15
+"""
 if is_ponding(water_height, elevation_height, 6,7):
     is_ponding_action(water_height, elevation_height, 6,7)
+"""
+if is_spreading(water_height, elevation_height, 2, 1):
+    is_spreading_action(water_height, elevation_height, 2, 1)
