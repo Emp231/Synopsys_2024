bb8996f4b842ab8b071a6b4278ddd23d81f5c64e
Samanyu Shrivastava <samshri06@gmail.com>
2024-01-04 12:26:08 -0800

    update

--- algorithms/conversion.py
+++ algorithms/conversion.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-input_tif = "C:/Users/saman/OneDrive/Documents/GitHub/Synopsys_2024/algorithms/spacereserve.asc"
-with open(input_tif, 'r') as file:
+input_asc = "" # PATH TO INPUT .asc (to read)
+with open(input_asc, 'r') as file:
     for _ in range(6):
         content = file.readline()
         print(content)
