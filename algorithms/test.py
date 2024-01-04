f6803c602a3f01c1a178ff08da66e8bed1106739
Samanyu Shrivastava <samshri06@gmail.com>
2024-01-04 14:07:28 -0800

    reading .asc file

---
+++ algorithms/test.py
@@ -0,0 +1,13 @@
+import caffé
+import numpy as np
+
+input_asc = "" # Add directory to .asc file
+
+def set_elevation_height(input_asc):
+    with open(input_asc, 'r') as file:
+        for _ in range(6):
+            file.readline()
+        
+        data = np.loadtxt(file)
+    
+    return data
