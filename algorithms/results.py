30b4544aa4fceb326b991fdc5a23b55c20a46dcb
Prasmit Kondawar <kondawarprasmit@gmail.com>
2024-03-05 00:15:55 -0800

    started testing of the fast floods

---
+++ algorithms/results.py
@@ -0,0 +1,12 @@
+import io
+import caffé
+import inputs
+
+#import cells of the caffe
+#import cells of fast flood
+#make equation to compare the two (percent error)
+
+
+def gettingCellsFF():
+    
+    
